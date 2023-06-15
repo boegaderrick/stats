118c96b377814c1c48a9bf58ff1ee7749e2fc406
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-06-15 03:50:39 +0300

    repo cleanup

--- app/stats.py
+++ app/stats.py
@@ -6,24 +6,28 @@ from src import storage
 
 app = Flask(__name__)
 
+
 @app.route('/stats', strict_slashes=False)
 def launch_app():
     """This function returns the landing page"""
     return render_template('landing.html')
+
 
 @app.route('/stats/home', strict_slashes=False)
 def home():
     """This function returns the home page"""
     return render_template('home.html')
 
+
 @app.route('/stats/api/athletes', strict_slashes=False)
 def get_athletes():
     """This function returns json representation of athlete objects"""
     ret = []
-    types =['F1Driver']
+    types = ['F1Driver']
     for typ in types:
         ret.extend([i.get_dict() for i in storage.get(typ)])
     return make_response(jsonify(ret))
+
 
 @app.route('/stats/athletes/<type>/<id>', strict_slashes=False)
 def get_athlete(type, id):
@@ -34,11 +38,13 @@ def get_athlete(type, id):
     athlete = storage.get(type, id).get_dict()
     return render_template('object.html', athlete=athlete)
 
+
 @app.route('/stats/api/teams', strict_slashes=False)
 def get_teams():
     """This function returns all team objects in json format"""
     teams = [i.get_dict() for i in storage.get('Team')]
     return make_response(jsonify(teams))
+
 
 @app.route('/stats/api/sports', strict_slashes=False)
 def get_sports():
@@ -46,10 +52,12 @@ def get_sports():
     sports = [i.get_dict() for i in storage.get('Sport')]
     return make_response(jsonify(sports))
 
+
 @app.teardown_appcontext
 def session_end(Exception=None):
     """This function closes the session object"""
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5005', debug=True)
